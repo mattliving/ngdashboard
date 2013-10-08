b2f1882604082f9ad3b742bc6a1d3273f8fe4890
Matthew Livingston <matt.l.livingston@gmail.com>
2013-10-08 14:33:36 +0100

    Basic login form template & initial passport integration.

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,11 @@
+angular.module("luckyDashApp").controller("LoginCtrl", function($window, $scope, $routeParams) {
+
+  angular.element('html').css('overflow', 'hidden');
+  angular.element('body').css('overflow', 'hidden');
+
+  $scope.height = 0;
+  $scope.$watch('height', function(newVal, oldVal) {
+    console.log(newVal);
+    $('.login').css('margin-top', newVal/4);
+  });
+});
